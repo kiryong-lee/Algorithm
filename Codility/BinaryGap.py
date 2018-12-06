66a23d5862d06e76dcb9f740043985aab0939247
kiryong_lee <kiryong_lee@tmax.co.kr>
2018-12-06 14:26:43 +0900

    Iterations - BinaryGap

---
+++ Codility/BinaryGap.py
@@ -0,0 +1,30 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):
+    # write your code in Python 3.6
+
+    arr = []
+    idx = 0
+    idx_arr = []
+    while N > 1:
+        binary = N % 2
+        arr.append(binary)
+        N = N // 2
+        if binary == 1:
+            idx_arr.append(idx)
+        idx += 1
+    if N == 1:
+        arr.append(1)
+        idx_arr.append(idx)
+
+    #print(arr)
+    #print(idx_arr)
+
+    max = 0
+    for i in range(1, len(idx_arr)):
+        if max < idx_arr[i] - idx_arr[i - 1] - 1:
+            max = idx_arr[i] - idx_arr[i - 1] - 1
+
+    #print(max)
+    return max
