34d0626632275770f7b5a352b2a0d06a2dfa3b52
Kiryong Lee <ebugger23@gmail.com>
2021-10-28 18:52:33 +0900

    Create 12865.py

---
+++ baekjoon/12865.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+max_value = [[0] * (K + 1) for i in range(N)]
+for i in range(N):
+    weight, value = map(int, input().split())
+    for j in range(K + 1):
+        if j < weight:
+            # max_value[-1][j] : no problem because of 0
+            max_value[i][j] = max_value[i - 1][j]
+        else:
+            max_value[i][j] = max(max_value[i - 1][j], value + max_value[i - 1][j - weight])
+
+print(max_value[N - 1][K])
