31382cbca9f614b07e308a8eaf68bb3bae0a3c62
Kiryong Lee <ebugger23@gmail.com>
2021-10-29 21:47:54 +0900

    Create 87946.py

---
+++ programmers/87946.py
@@ -0,0 +1,18 @@
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    dungeon_perm = permutations(dungeons)
+    answer = 0
+    for dungeon_list in list(dungeon_perm):
+        limit = k
+        current_clear = 0
+        for dungeon in dungeon_list:
+            if limit >= dungeon[0]:
+                limit -= dungeon[1]
+                current_clear += 1
+        
+        if answer < current_clear:
+            answer = current_clear
+    
+    return answer
