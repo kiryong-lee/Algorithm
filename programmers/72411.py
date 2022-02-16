0a1b562a3a19e4b685185b01165dd327e136f631
Kiryong Lee <ebugger23@gmail.com>
2022-02-16 21:12:54 +0900

    2021 KAKAO BLIND RECRUITMENT > 메뉴 리뉴얼
    불필요한 코드 정리

--- programmers/72411.py
+++ programmers/72411.py
@@ -3,32 +3,26 @@ import itertools
 
 def solution(orders, course):
     n = len(orders)
-    dictionary = dict()
-    for c in course:
-        for i in range(n):
-            for j in range(i + 1, n):
-                common = set(orders[i]).intersection(set(orders[j]))
-                if len(common) >= c:
-                    for menu in itertools.combinations(common, c):
-                        str_value = ''.join(sorted(list(menu)))
-                        if str_value in dictionary:
-                            dictionary[str_value] = dictionary[str_value] + 1
-                        else:
-                            dictionary[str_value] = 1
-    
     answer = []
     for c in course:
+        dictionary = dict()
+        for order in orders:
+            for menu in itertools.combinations(order, c):
+                str_value = ''.join(sorted(list(menu)))
+                if str_value in dictionary:
+                    dictionary[str_value] = dictionary[str_value] + 1
+                else:
+                    dictionary[str_value] = 1
+
         small_answer = []
-        small_max = 0
+        small_max = 2
         for menu in list(dictionary):
-            if len(menu) == c:
-                if dictionary[menu] > small_max:
-                    small_max = dictionary[menu]
-                    small_answer = [menu]
-                elif dictionary[menu] == small_max:
-                    small_answer.append(menu)
-        
-        for menu in small_answer:
-            answer.append(menu)
-    
+            if dictionary[menu] > small_max:
+                small_max = dictionary[menu]
+                small_answer = [menu]
+            elif dictionary[menu] == small_max:
+                small_answer.append(menu)
+
+        answer += small_answer
+
     return sorted(answer)
