7f8de7cc4382e8918c473bc82ac00eccfa132136
Kiryong Lee <ebugger23@gmail.com>
2022-03-04 20:37:20 +0900

    Summer/Winter Coding(~2018) > 스킬트리

--- programmers/49993.py
+++ programmers/49993.py
@@ -3,17 +3,14 @@ def solution(skill, skill_trees):
     possible = 0
     
     for skill_tree in skill_trees:
-        i, j = 0, 0
         order = ''
-        while j < len(skill_tree):
-            if skill_tree[j] in skill:
-                order += skill_tree[j]
+        for s in skill_tree:
+            if s in skill:
+                order += s
             
             if len(order) == len(skill):
                 break
             
-            j += 1
-        
         if order == skill[:len(order)]:
             possible += 1
     
