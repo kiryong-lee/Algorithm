b543fae4f46ceadf7819801abd61fbf69a5b4739
Kiryong Lee <ebugger23@gmail.com>
2022-03-07 20:15:25 +0900

    Winter Coding(~2018) > 기지국 설치

---
+++ programmers/Summer/12979.py
@@ -0,0 +1,20 @@
+
+import math
+
+def solution(n, stations, w):
+    count = 0
+    interval = 2 * w + 1
+    distance = stations[0] - w - 1
+    if distance > 0:
+        count = math.ceil(distance / interval)
+
+    for i in range(1, len(stations)):
+        distance = stations[i] - stations[i - 1] - interval
+        if distance > 0:
+            count += math.ceil(distance / interval)
+
+    distance = n - stations[-1] - w
+    if distance > 0:
+        count += math.ceil(distance / interval)
+
+    return count
