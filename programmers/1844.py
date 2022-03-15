3f359fbfffcc9f124ee0247987f61de8e4fac913
Kiryong Lee <ebugger23@gmail.com>
2022-03-15 19:59:19 +0900

    찾아라 프로그래밍 마에스터 > 게임 맵 최단거리

--- programmers/1844.py
+++ programmers/1844.py
@@ -7,16 +7,16 @@ def solution(maps):
 
     length_dict = dict()
     length_dict[(0, 0)] = 1
-    stack = [(0, 0)]
+    queue = [(0, 0)]
 
-    while stack:
-        x, y = stack.pop(0)
+    while queue:
+        x, y = queue.pop(0)
         
         for xp, yp in xy:
             if 0 <= x + xp <= n and 0 <= y + yp <= m and maps[y + yp][x + xp] == 1:
                 if (x + xp, y + yp) not in length_dict:
                     length_dict[(x + xp, y + yp)] = length_dict[(x, y)] + 1
-                    stack.append((x + xp, y + yp))
+                    queue.append((x + xp, y + yp))
 
         if (n, m) in length_dict:
             return length_dict[(n, m)]
